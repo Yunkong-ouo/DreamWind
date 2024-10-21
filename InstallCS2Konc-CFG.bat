3b3e510aa14a40e8c110865ca1076c9524ed9fcd
Yunkong-ouo <58383585+Yunkong-ouo@users.noreply.github.com>
2024-10-21 21:58:18 +0800

    自動安奘更新

--- InstallCS2Konc-CFG.bat
+++ InstallCS2Konc-CFG.bat
@@ -5,15 +5,15 @@ REM 檢查系統是否為 64 位，並設置相應路徑
 if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
 
 REM 檢查是否具有管理員權限
-bcdedit >nul
-if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+REM bcdedit >nul
+REM if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
 
-:UACPrompt
-%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
-exit /B
+REM :UACPrompt
+REM %1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+REM exit /B
 
-:UACAdmin
-cd /d "%~dp0"
+REM :UACAdmin
+REM cd /d "%~dp0"
 
 REM 檢查是否在 CS2Konc-CFG 文件夾中
 for %%I in (.) do set CurrDirName=%%~nxI
@@ -22,9 +22,30 @@ if /I not "%CurrDirName%"=="CS2Konc-CFG" (
     echo 請把此文件放進 CS2Konc-CFG 文件夾中!!!
     echo.請確保此文件在 *\Counter-Strike Global Offensive\game\csgo\cfg\CS2Konc-CFG 目錄當中
     echo.
-    pause
+	echo 請按任意鍵退出。
+    pause >nul
+    exit /b
+)
+
+REM 執行當前目錄的 resourceZIP.bat
+if exist "%~dp0\install\resourceZIP.bat" (
+    echo 正在執行 resourceZIP.bat...
+    call "%~dp0\install\resourceZIP.bat"
+    if errorlevel 1 (
+        echo 執行 resourceZIP.bat 失敗，停止腳本。
+		echo 請按任意鍵退出。
+        pause >nul
+        exit /b
+    )
+) else (
+    echo 未找到 resourceZIP.bat，請確認它存在於當前目錄中。
+	echo 請按任意鍵退出。
+    pause >nul
     exit /b
 )
 
 REM 執行默認的 PowerShell 腳本
 call powershell.exe -ExecutionPolicy Bypass -File ".\install\CS2Konc-CFG_install.ps1"
+
+::echo 請按任意鍵退出。
+::pause >nul
--- install/CS2Konc-CFG_install.ps1
+++ install/CS2Konc-CFG_install.ps1
@@ -76,8 +76,8 @@ if (Test-Path $autoexecPath) {
     # 讀取現有內容
     $existingContent = Get-Content -Path $autoexecPath
     
-    # 移除重複的 exec CS2Konc-CFG/CSKoncMod
-    $existingContent = $existingContent | Where-Object { $_ -ne $content }
+    # 使用正則表達式移除包含 exec CS2Konc-CFG/CSKoncMod 的行（不管前後有什麼）
+    $existingContent = $existingContent | Where-Object { $_ -notmatch ".*exec\s+CS2Konc-CFG/CSKoncMod.*" }
     
     # 將新內容添加到現有內容的前面
     $newContent = @($content) + $existingContent
