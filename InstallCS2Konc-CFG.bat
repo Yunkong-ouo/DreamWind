ca57725f690d58a20641502aacfec8faef994b7b
Yunkong-ouo <58383585+Yunkong-ouo@users.noreply.github.com>
2024-10-22 23:50:32 +0800

    優化自動安裝

--- InstallCS2Konc-CFG.bat
+++ InstallCS2Konc-CFG.bat
@@ -1,48 +1,106 @@
 @echo off
 chcp 65001 >nul 2>&1
 
-REM 檢查系統是否為 64 位，並設置相應路徑
+REM 檢查 64 位系統路徑
 if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
 
-REM 檢查是否在 CS2Konc-CFG 文件夾中
+REM 跳過管理員權限
+goto awa
+
+REM 確認是否有管理員權限
+bcdedit >nul
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else (
+    goto UACAdmin
+)
+
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+
+:UACAdmin
+cd /d "%~dp0"
+
+:awa
+REM 檢查是否在 CS2Konc-CFG 資料夾中
 for %%I in (.) do set CurrDirName=%%~nxI
 if /I not "%CurrDirName%"=="CS2Konc-CFG" (
+    cls
     color 0C
-    echo 請把此文件放進 CS2Konc-CFG 文件夾中!!!
-    echo.請確保此文件在 *\Counter-Strike Global Offensive\game\csgo\cfg\CS2Konc-CFG 目錄當中
+    echo 請把此檔案放進 CS2Konc-CFG 資料夾中!!!
+    echo 請把此檔案放進 CS2Konc-CFG 資料夾中!!!
+    echo 請把此檔案放進 CS2Konc-CFG 資料夾中!!!
+    echo 請確保此檔案在 *\Counter-Strike Global Offensive\game\csgo\cfg\CS2Konc-CFG 目錄當中
     echo.
-    echo 請按任意鍵退出。
+    echo.請按任意鍵退出。
     pause >nul
     exit /b
 )
 
-REM 壓縮 resource 文件夾為 resource.zip
+REM 壓縮 resource 資料夾為 resource.zip
+cls
+color 0a
 set zipFile=resource.zip
 set folderToZip=%cd%\resource
 
-REM 如果已經有同名的 zip 檔案，先刪除
+REM 如果已存在同名的 zip 檔案，先刪除
 if exist "%zipFile%" del "%zipFile%"
 if exist "%folderToZip%" (
-    echo 正在壓縮 resource 文件夾...
+    echo 正在壓縮 resource 資料夾...
     powershell.exe -Command "Compress-Archive -Path '%folderToZip%\*' -DestinationPath '%zipFile%' -Force"
-    REM "C:\Program Files\7-Zip\7z.exe" a "%zipFile%" -r "%folderToZip%\*"
-    if errorlevel 1 (
+
+    REM 檢查壓縮檔案是否存在
+    if exist "%zipFile%" (
+        echo 壓縮完成: %zipFile%
+    ) else (
         echo 壓縮失敗，停止腳本。
-        echo 請按任意鍵退出。
+        echo.請按任意鍵退出。
         pause >nul
         exit /b
-    ) else (
-        echo 壓縮完成: %zipFile%
     )
 ) else (
-    echo resource 文件夾不存在，無法壓縮。
-    echo 請按任意鍵退出。
+    echo resource 資料夾不存在，無法壓縮。
+    echo.
+    echo.請按任意鍵退出。
     pause >nul
     exit /b
 )
 
-REM 執行默認的 PowerShell 腳本
-call powershell.exe -ExecutionPolicy Bypass -File ".\install\CS2Konc-CFG_install.ps1"
+REM 檢查 "完美世界竞技平台" 進程是否運行
+tasklist /FI "IMAGENAME eq 完美世界竞技平台.exe" 2>nul | find /I /N "完美世界竞技平台.exe">nul
+if "%ERRORLEVEL%"=="1" (
+    cls
+    color 0C
+    echo 如果你玩完美平台，請按N退出程序，運行完美對戰平台後啟動此檔案!!!
+	echo 如果你玩完美平台，請按N退出程序，運行完美對戰平台後啟動此檔案!!!
+	echo 如果你玩完美平台，請按N退出程序，運行完美對戰平台後啟動此檔案!!!
+	echo.
+	echo 再次確認
+    echo 如果你确定不玩完美平台，然後按Y繼續
+    echo.
+    choice /C YN /N /M "按 Y 繼續，按 N 退出"
+    if errorlevel 2 (
+        echo 你選擇了退出。
+		timeout /t 1 >nul
+        exit /b
+    ) else (
+        echo 你選擇了繼續。
+        timeout /t 1 >nul
+		cls
+    )
+    color 0A
+    REM 運行 PowerShell 腳本並在完成後退出
+    call powershell.exe -ExecutionPolicy Bypass -File ".\install\CS2Konc-CFG_install_Resource.ps1"
+    REM echo.請按任意鍵退出。
+    REM pause >nul
+    exit /b
+)
 
-:: echo 請按任意鍵退出。
-:: pause >nul
+cls
+color 0A
+REM 執行默認的 PowerShell 腳本
+call powershell.exe -ExecutionPolicy Bypass -File ".\install\CS2Konc-CFG_install_Resource_Perfect.ps1"
+REM echo.請按任意鍵退出。
+REM pause >nul
---
+++ install/CS2Konc-CFG_install_Resource.ps1
@@ -0,0 +1,88 @@
+﻿# 設置控制台編碼為 UTF-8 BOM
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding($true)
+
+# 啟用日誌
+$VerbosePreference = "Continue"
+
+# 導入必要的程序集並初始化窗口
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+# 添加鍵盤輔助程序類
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class KeyboardHelper {
+    [DllImport("user32.dll")]
+    public static extern short GetKeyState(int nVirtKey);
+}
+"@
+
+# 添加 Win32 類以處理窗口
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Win32 {
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+}
+"@
+
+# 全局變量
+$script:isBinding = $false
+$script:currentBindingButton = $null
+
+# 按鍵綁定控件 UI 參數
+$script:bindingControlHeight = 28
+$script:bindingControlWidth = 110
+$script:bindingControlHorizontalSpacing = 5
+$script:bindingControlVerticalSpacing = 5
+
+# 獲取當前目錄
+$currentDirectory = Get-Location
+$sourceFile = Join-Path -Path $currentDirectory -ChildPath "resource.zip"
+# 檢查 resource.zip 是否存在，並使用消息框顯示結果
+if (-Not (Test-Path -Path $sourceFile)) {
+    [System.Windows.Forms.MessageBox]::Show("當前目錄下未找到 resource.zip 文件", "錯誤", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+    exit
+}
+
+# 定義解壓縮目標路徑
+$zipFilePath2 = ".\resource.zip"
+$destinationFolder2 = "..\..\resource"
+# 檢查目標資料夾是否存在，若不存在則創建
+if (-not (Test-Path $destinationFolder2)) {
+    New-Item -Path $destinationFolder2 -ItemType Directory
+}
+else {
+    # 刪除檔案，保留名為 game-icon.bmp 的檔案
+    Get-ChildItem -Path $destinationFolder2 -Recurse | ForEach-Object {
+        if ($_.Name -ne "game-icon.bmp") {
+            try {
+                Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction Stop
+            }
+            catch {
+                Write-Host "無法刪除項目：$($_.FullName) - $_"
+            }
+        }
+    }
+}
+# 解壓縮文件
+Expand-Archive -Path $zipFilePath2 -DestinationPath $destinationFolder2 -Force
+
+# 創建Autoexec
+& ".\install\CS2Konc-CFG_Autoexec.ps1"
+
+# 輸出安裝完成消息
+Write-Verbose "安裝完成"
+# 最終提示
+$finalMessage = "安裝完成，請重啟遊戲後測試。`n`n"
+if ($feedbackMessage) {
+    $finalMessage += "附註：`n$feedbackMessage"
+}
+# 顯示安裝完成的消息框
+$result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "安裝完成", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
