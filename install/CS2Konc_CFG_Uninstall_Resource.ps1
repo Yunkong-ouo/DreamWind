d571640d9bc12c1579173f82d8421a569847b8df
Yunkong-ouo <58383585+Yunkong-ouo@users.noreply.github.com>
2024-11-11 05:51:47 +0800

    更新靜步縮放小地圖跟修復自動安裝bug

--- install/CS2Konc_CFG_Uninstall_Resource.ps1
+++ install/CS2Konc_CFG_Uninstall_Resource.ps1
@@ -71,12 +71,12 @@ else {
 
 # 設定系統語言
 $systemLanguage = [System.Globalization.CultureInfo]::CurrentCulture.Name
+Write-Host "$systemLanguage"
 
 # 根據語言設置顯示不同的提示信息
 if ($systemLanguage -eq "zh-CN") {
     # 簡體中文提示
     $finalMessage = "解除安装完成，请重启游戏后测试。`n`n"
-    Write-Host "$systemLanguage"
     $verboseMessage = "解除安装完成"
     Write-Host $verboseMessage
     # 顯示安裝的消息框（簡體中文）
@@ -84,7 +84,6 @@ if ($systemLanguage -eq "zh-CN") {
 } elseif ($systemLanguage -eq "zh-TW") {
     # 繁體中文提示
     $finalMessage = "解除安裝完成，請重啟遊戲後測試。`n`n"
-    Write-Host "$systemLanguage"
     $verboseMessage = "解除安裝完成"
     Write-Host $verboseMessage
     # 顯示安裝的消息框（繁體中文）
@@ -92,7 +91,6 @@ if ($systemLanguage -eq "zh-CN") {
 } elseif ($systemLanguage -eq "en-US") {
     # 英文提示
     $finalMessage = "Uninstallation complete. Please restart the game and test. `n`n"
-    Write-Host "$systemLanguage"
     $verboseMessage = "Uninstallation completed."
     Write-Host $verboseMessage
     # 顯示安裝的消息框（英文）
@@ -100,7 +98,6 @@ if ($systemLanguage -eq "zh-CN") {
 } else {
     # 預設語言提示（繁體中文）
     $finalMessage = "安裝完成，請重啟遊戲後測試。`n`n"
-    Write-Host "$systemLanguage"
     $verboseMessage = "安裝完成"
     Write-Host $verboseMessage
     # 顯示安裝的消息框（預設，繁體中文）
--- install/CS2Konc_CFG_install_Resource.ps1
+++ install/CS2Konc_CFG_install_Resource.ps1
@@ -77,31 +77,32 @@ Expand-Archive -Path $zipFilePath2 -DestinationPath $destinationFolder2 -Force
 # 創建Autoexec
 & ".\install\CS2Konc_CFG_Autoexec.ps1"
 
+# 設定系統語言
+$systemLanguage = [System.Globalization.CultureInfo]::CurrentCulture.Name
+Write-Host "$systemLanguage"
+
 # 根據語言設置顯示不同的提示信息
 if ($systemLanguage -eq "zh-CN") {
     # 簡體中文提示
-    $finalMessage = "安装完成，请重启游戏后测试。`n`n"
-    Write-Host "$systemLanguage"
-    $verboseMessage = "安装完成"
+    $finalMessage = "解除安装完成，请重启游戏后测试。`n`n"
+    $verboseMessage = "解除安装完成"
     Write-Host $verboseMessage
     # 顯示安裝的消息框（簡體中文）
-    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "安装完成", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "解除安装完成", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 } elseif ($systemLanguage -eq "zh-TW") {
     # 繁體中文提示
-    $finalMessage = "安裝完成，請重啟遊戲後測試。`n`n"
-    Write-Host "$systemLanguage"
-    $verboseMessage = "安裝完成"
+    $finalMessage = "解除安裝完成，請重啟遊戲後測試。`n`n"
+    $verboseMessage = "解除安裝完成"
     Write-Host $verboseMessage
     # 顯示安裝的消息框（繁體中文）
-    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "安裝完成", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "解除安裝完成", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 } elseif ($systemLanguage -eq "en-US") {
     # 英文提示
-    $finalMessage = "install completed. Please restart the game and test. `n`n"
-    Write-Host "$systemLanguage"
-    $verboseMessage = "install Completed"
+    $finalMessage = "Uninstallation complete. Please restart the game and test. `n`n"
+    $verboseMessage = "Uninstallation completed."
     Write-Host $verboseMessage
     # 顯示安裝的消息框（英文）
-    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "install Completed", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    $result = [System.Windows.Forms.MessageBox]::Show($finalMessage, "Uninstallation completed.", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 } else {
     # 預設語言提示（繁體中文）
     $finalMessage = "安裝完成，請重啟遊戲後測試。`n`n"
