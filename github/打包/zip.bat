cecabe1e072b74d5ab97de7ebeaf7d3d63afd025
Yunkong-ouo <58383585+Yunkong-ouo@users.noreply.github.com>
2025-01-27 12:56:26 +0800

    1.2.8

--- github/打包/zip.bat
+++ github/打包/zip.bat
@@ -3,18 +3,19 @@ chcp 65001 >nul 2>&1
 cls
 color 0a
 
-set zipFile=CS2Konc_CFG.zip
-REM set zipFile=CS2Konc_CFG.7z
+set version=v1.2.8
+set zipFile=CS2Konc_CFG_%version%.zip
+REM set zipFile=CS2Konc_CFG_%version%.7z
 set folderToZip=CS2Konc_CFG
 
-REM 如果已存在同名的 7z 檔案，先刪除
+REM 如果已存在同名的壓縮檔案，先刪除
 if exist "%zipFile%" del "%zipFile%"
 
 if exist "%folderToZip%" (
-    echo 正在壓縮 CS2Konc_CFG 資料夾...
-    REM 使用 7-Zip 壓縮資料夾為 7z 格式，並設置 LZMA 壓縮方法
+    echo 正在壓縮 %folderToZip% 資料夾為版本 %version% ...
+    REM 使用 7-Zip 壓縮資料夾
     REM "C:\Program Files\7-Zip\7z.exe" a -t7z "%zipFile%" "%folderToZip%\*" -mx9 -m0=LZMA -md=256m -ms=on -mfb=273
-	"C:\Program Files\7-Zip\7z.exe" a -tzip "%zipFile%" "%folderToZip%\*" -mx9 -mm=Deflate64
+    "C:\Program Files\7-Zip\7z.exe" a -tzip "%zipFile%" "%folderToZip%\*" -mx9 -mm=Deflate64
 
     REM 檢查壓縮檔案是否存在
     if exist "%zipFile%" (
@@ -25,8 +26,9 @@ if exist "%folderToZip%" (
         exit /b
     )
 ) else (
-    echo CS2Konc_CFG 資料夾不存在，無法壓縮。
+    echo %folderToZip% 資料夾不存在，無法壓縮。
     pause >nul
     exit /b
 )
 pause
+exit /b
